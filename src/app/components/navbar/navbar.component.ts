c46409a6c7d017ad7bd61660fb143c08331b832c
Rafael Garcia <rafa.garcia.leon@gmail.com>
2018-08-17 00:08:59 +0200

    Login y logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public isLogin: boolean;
+  public nombreUser: string;
+  public email: string;
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe( auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.nombreUser = auth.displayName;
+        this.email = auth.email;
+      } else {
+        this.isLogin = false;
+      }
+    });
   }
 
+  onClickLogout () {
+    this.authService.logout();
+  }
 }
